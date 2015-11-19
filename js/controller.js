797c5d3bfabe02456a6fdebfc93821666d8e33da
FreddyCalk <freddycalk@gmail.com>
2015-11-19 11:48:24 -0500

    twitter api
    controller set up

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,7 @@
-controller.js
+angular.module('twitterApp',[]).controller('twitterController',function($scope,$http){
+	var apiKey = 'xlAgMS5Vz808jUe429KWaV7vyrtaEEpFas0>B';
+	var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id='+apiKey;
+	$http.get(twitterURL).success(function(tweetData){
+		console.log(tweetData);
+	})
+})
