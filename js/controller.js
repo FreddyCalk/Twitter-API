05451c32bc74e9b2a6b2fd3ef19f92a7d41d483a
FreddyCalk <freddycalk@gmail.com>
2015-11-19 15:43:41 -0500

    trump tweets
    other commit

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,21 @@
-var twitterApp = angular.module('twitterApp',[]);
+var twitterApp = angular.module('twitterApp',['ngRoute']);
 
-	// twitterApp.config(function($routeProvider){
-	// 	$routeProvider.when('/',{
-	// 		templateURL: 'index.html',
-	// 		controller: 'twitterController'
-	// 	}).
-	// 	when('/:firstParam',{
-	// 		templateURL: 'pages/otherTweets.html',
-	// 		controller: 'otherController'
-	// 	}).
-	// 	otherwise({
-	// 		redirectTo: '/'
-	// 	});
-	// })
+	twitterApp.config(function($routeProvider){
+		$routeProvider.when('/',{
+			templateURL: 'index.html',
+			controller: 'twitterController'
+		}).
+		when('/:firstParam',{
+			templateURL: 'pages/otherTweets.html',
+			controller: 'otherController'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	})
 
 	twitterApp.controller('twitterController',function ($scope,$http){
-
+		
 		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=women';	
 			$http.get(twitterURL).success(function (tweetData){
 				console.log(tweetData);
@@ -24,11 +24,15 @@ var twitterApp = angular.module('twitterApp',[]);
 			})
 	})
 
-// twitterApp.controller('otherController', function($scope,$http, $routeParams){
-// 	var firstParam = $routeParams.firstParam;
-	
-	
-// })
+twitterApp.controller('otherController', function($scope,$http, $routeParams){
+		var searchVar = $routeParams.firstParam;
+		var secondVar = '&secondHash='+searchVar;
+		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump';
+		$http.get(twitterURL).success(function (tweetData){
+			console.log(tweetData);
+
+		})
+})
 
 
 
