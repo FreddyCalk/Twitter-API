d2441ada1b785b6644e08f845c8980a14ed51716
FreddyCalk <freddycalk@gmail.com>
2015-11-19 13:31:25 -0500

    Trump Tweets
    set up the routing logic within the JavaScript file

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,46 @@
 var myApp = angular.module('twitterApp',[])
 
-myApp.controller('twitterController',function ($scope,$http){
-	var apiKey = 'xlAgMS5Vz808jUe429KWaV7vyrtaEEpFas0>B';
-	var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id='+apiKey;
-	console.log(twitterURL)
-	$http.get(twitterURL).success(function (tweetData){
-		console.log(tweetData);
-
-	})
+twitterApp.config(function($routeProvider){
+	%routeProvider.when('/',{
+		templateURL: 'index.html',
+		controller: 'twitterController'
+	}).
+	when('/:firstParam',{
+		templateURL: 'pages/otherTweets.html',
+		controller: 'otherController'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
 })
+
+myApp.controller('twitterController',twitterController)
+
+
+	var twitterController =	function ($scope,$http){
+	var apiKey = 'xlAgMS5Vz808jUe429KWaV7vyrtaEEpFas0>B';
+	var searchTerm = "trump";
+	var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23'+searchTerm+'&since_id='+apiKey;
+	console.log(twitterURL)
+		$http.get(twitterURL).success(function (tweetData){
+			console.log(tweetData);
+			$scope.tweetText = tweetData.text;
+			$scope.tweetUser = tweetData.user;
+			$scope.
+		})
+	}
+myApp.controller('otherController', function($scope,$http, $routeParams){
+	var firstParam = $routeParams.firstParam;
+	
+	
+})
+
+
+
+
+
+
+
+
+
+
