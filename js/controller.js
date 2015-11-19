9a047c144cb8cb6fa8ba55c0d7e98dc4193eb1a6
FreddyCalk <freddycalk@gmail.com>
2015-11-19 16:47:57 -0500

    Trump Tweets
    added routing so that it works better

--- js/controller.js
+++ js/controller.js
@@ -2,37 +2,34 @@ var twitterApp = angular.module('twitterApp',['ngRoute']);
 
 	twitterApp.config(function($routeProvider){
 		$routeProvider.when('/',{
-			templateURL: 'index.html',
+			templateUrl: 'pages/default-tweets.html',
 			controller: 'twitterController'
 		}).
 		when('/:firstParam',{
-			templateURL: 'pages/otherTweets.html',
+			templateUrl: 'pages/other-tweets.html',
 			controller: 'otherController'
 		}).
 		otherwise({
 			redirectTo: '/'
 		});
-	})
-
-	twitterApp.controller('twitterController',function ($scope,$http){
+	});
+	twitterApp.controller('twitterController',function ($scope, $http){
 		
-		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=women';	
+		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump';	
 			$http.get(twitterURL).success(function (tweetData){
-				console.log(tweetData);
 				$scope.tweets = tweetData.statuses;
-
 			})
 	})
 
-twitterApp.controller('otherController', function($scope,$http, $routeParams){
+	twitterApp.controller('otherController', function($scope,$http, $routeParams){
 		var searchVar = $routeParams.firstParam;
 		var secondVar = '&secondHash='+searchVar;
-		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump';
+		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump'+secondVar;
 		$http.get(twitterURL).success(function (tweetData){
-			console.log(tweetData);
+			$scope.tweets = tweetData.statuses;
 
 		})
-})
+	})
 
 
 
