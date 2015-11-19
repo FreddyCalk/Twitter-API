18bc5123c9fa985d75988ad63ae6a7108aa0793d
FreddyCalk <freddycalk@gmail.com>
2015-11-19 13:09:25 -0500

    twitter api
    typo fix

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,11 @@
-angular.module('twitterApp',[]).controller('twitterController',function($scope,$http){
+var myApp = angular.module('twitterApp',[])
+
+myApp.controller('twitterController',function ($scope,$http){
 	var apiKey = 'xlAgMS5Vz808jUe429KWaV7vyrtaEEpFas0>B';
 	var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id='+apiKey;
-	$http.get(twitterURL).success(function(tweetData){
+	console.log(twitterURL)
+	$http.get(twitterURL).success(function (tweetData){
 		console.log(tweetData);
+
 	})
 })
