9b3cec7f2e8d6b748ea9f8ff736035ff5479b953
Freddy Calk <freddycalk@gmail.com>
2016-01-31 13:43:19 -0500

    added search function to the nav bar
    from home page will just search twitter, not perfect, but a better
    feature than before

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,5 @@
+var currentParam;
+
 var twitterApp = angular.module('twitterApp',['ngRoute','ngCookies']);
 
 	twitterApp.config(function($routeProvider){
@@ -19,13 +21,18 @@ var twitterApp = angular.module('twitterApp',['ngRoute','ngCookies']);
 	});
 	twitterApp.controller('twitterController', function ($scope,$http){
 		
-		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump';	
+		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=clinton';	
 			$http.get(twitterURL).success(function (tweetData){
 				$scope.tweets = tweetData.statuses;
 				console.log($scope.tweets)
+				var blanksFound = 0;
 				for (i=0;i<tweetData.statuses.length;i++) {
-					if($scope.tweets[i].user.profile_banner_url == null) {
+					if(($scope.tweets[i].user.profile_banner_url == null)&&(blanksFound%2 == 0)) {
 						$scope.tweets[i].user.profile_banner_url = "assets/images/sample-cover.jpg";
+						blanksFound++;
+					}else if(($scope.tweets[i].user.profile_banner_url == null)&&(blanksFound%2 == 1)) {
+						$scope.tweets[i].user.profile_banner_url = "assets/images/Hallary-Clinton-2015-Banner.jpg";
+						blanksFound++;
 					}
 				};
 			});
@@ -33,14 +40,21 @@ var twitterApp = angular.module('twitterApp',['ngRoute','ngCookies']);
 
 	twitterApp.controller('otherController', function ($scope, $http, $routeParams){
 		var searchVar = $routeParams.firstParam;
+		currentParam = searchVar;
 		var twitterURL = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash='+searchVar;
 		$http.get(twitterURL).success(function (tweetData){
 			$scope.tweets = tweetData.statuses;
 			for(i=0;i<tweetData.statuses.length;i++){
-				if($scope.tweets[i].user.profile_banner_url == null) {
+				if(($scope.tweets[i].user.profile_banner_url == null)&&(searchVar == 'trump')) {
 					$scope.tweets[i].user.profile_banner_url = "assets/images/sample-cover.jpg";
+				}else if(($scope.tweets[i].user.profile_banner_url == null)&&(searchVar == 'clinton')) {
+					$scope.tweets[i].user.profile_banner_url = "assets/images/Hallary-Clinton-2015-Banner.jpg";
 				}
 			}
+		$scope.submit = function(){
+			console.log($scope.text);
+		}
+
 		});
 	});
 
@@ -51,13 +65,31 @@ var twitterApp = angular.module('twitterApp',['ngRoute','ngCookies']);
 		$http.get(twitterURL).success(function (tweetData){
 			$scope.tweets = tweetData.statuses;
 			for(i=0;i<tweetData.statuses.length;i++){
-				if($scope.tweets[i].user.profile_banner_url == null) {
+				if(($scope.tweets[i].user.profile_banner_url == null)&&(searchVar == 'trump')) {
 					$scope.tweets[i].user.profile_banner_url = "assets/images/sample-cover.jpg";
+				}else if(($scope.tweets[i].user.profile_banner_url == null)&&(searchVar == 'clinton')) {
+					$scope.tweets[i].user.profile_banner_url = "assets/images/Hallary-Clinton-2015-Banner.jpg";
 				}
 			}
 
 		})
 	});
+
+	twitterApp.controller('search-controller', function ($scope){
+		$scope.submit = function(){
+			var searchTerm = $scope.text.split(" ");
+			if((currentParam)&&($scope.text)){
+				$('#search-field').val("");
+				window.location.href = "#/"+currentParam+"/"+searchTerm[0];
+			}else if($scope.text){
+				$('#search-field').val("");
+				window.location.href = "#/"+searchTerm[0];
+			}
+			currentParam = undefined;
+		}
+	})
+
+
 
 
 
